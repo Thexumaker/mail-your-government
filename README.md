f0001de7e37c78de51a293ec90e27650aff5e0c7
thexumaker <thexumaker@gmail.com>
2022-05-08 18:31:43 -0700

    more scripts

---
+++ README.md
@@ -0,0 +1,23 @@
+## Mail your Government Script
+
+This is a demo script of how you could send a postcard to your governement representatives anonymously.
+
+
+## How to use this script
+
+Head to Lob.com and create an account, add a credit card and grab an API key.
+
+Then add your API key to the top of send_hundreds.js or send_single.js with your API key
+
+Run npm install
+
+
+Then if you want to send a single letter run 
+```
+node send_single.js
+```
+
+To send someone hundreds of letters:
+```
+no send_hundreds.js
+```
---
+++ send_hundreds.js
@@ -0,0 +1,46 @@
+const lob = require('lob')('test_7c87ce9e02905af890428abedc07dc64087');
+
+const fs = require('fs').promises;
+async function loadBack() {
+    const data = await fs.readFile("./assets/back/back.html", "binary");
+    return Buffer.from(data);
+}
+
+async function loadFront() {
+  const data = await fs.readFile("./assets/front/front.html", "binary");
+  return Buffer.from(data);
+}
+
+const address = {
+  name: 'Brett Kavanaugh',
+  address_line1: '3706 UNDERWOOD ST',
+  address_city: 'CHEVY CHASE',
+  address_state: 'MD',
+  address_zip: '20815'
+}
+
+const createPostcardPromise = async function() {
+  return lob.postcards.create({
+  description: 'Demo Postcards job',
+  to: address,
+  front: await loadFront(),
+  back: await loadBack(),
+  size: "6x9"
+})
+}
+
+async function sendPostcards() {
+  const postcards = []
+  for (let i =0; i < 2; i++) {
+    postcards.push(createPostcardPromise())
+  }
+  try {
+    await Promise.all(postcards)
+    console.log('success')
+  }
+  catch(e) {
+    console.log(e)
+  }
+}
+
+sendPostcards();
